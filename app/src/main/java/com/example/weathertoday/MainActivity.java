e0bb4c21c8e33c59286a4193975a8723ca34e31b
Алексей Рогатин <bamako99@mail.ru>
2021-01-13 12:09:28 +0300

    Немного изменен код получения данных из Intent

--- app/src/main/java/com/example/weathertoday/MainActivity.java
+++ app/src/main/java/com/example/weathertoday/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.weathertoday;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView windSpeed;
 
     private String currentWeatherPostfix = "\u00B0"; // В будущем будет выбираться согласно пользовательким настройкам
-    private String wikiUrl = "https://ru.wikipedia.org/wiki/";
+    private final String wikiUrl = "https://ru.wikipedia.org/wiki/";
 
     private static final int REQUEST_ACCESS_TYPE = 1;
     public static final String ACCESS_MESSAGE = "Location";
@@ -36,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.menuButton).setOnClickListener(v -> openMenu());
         findViewById(R.id.optionsButton).setOnClickListener(v -> openOptions());
         findViewById(R.id.locationInfoView).setOnClickListener(v -> openLocationInfo());
-
     }
 
     @Override
@@ -60,13 +60,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK) {
-            String location = data.getStringExtra(ACCESS_MESSAGE);
-            currentLocation.setText(location);
+        if (requestCode == REQUEST_ACCESS_TYPE) {
+            if (resultCode == RESULT_OK) {
+                String location = Objects.requireNonNull(data).getStringExtra(ACCESS_MESSAGE);
+                currentLocation.setText(location);
+                generateData();
+            } else {
+                currentLocation.setText(getResources().getString(R.string.error_message));
+            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-        generateData();
     }
 
     private void findViews() {
@@ -78,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void generateData() {
-
         String currentLocationValue = getResources().getString(R.string.weather_location);
         String temperatureValue = (int) (Math.random() * 25) + currentWeatherPostfix;
         String moistureValue = String.valueOf((int) (Math.random() * 100));
@@ -88,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
         setData(currentLocationValue, temperatureValue, moistureValue, pressureValue, windSpeedValue);
     }
 
-    @SuppressLint("SetTextI18n")
     private void setData(String currentLocationValue, String temperatureValue, String moistureValue, String pressureValue, String windSpeedValue) {
         if (currentLocation.getText().equals(getResources().getString(R.string.weather_location)))
             currentLocation.setText(currentLocationValue);
@@ -115,6 +117,5 @@ public class MainActivity extends AppCompatActivity {
             Intent browser = new Intent(Intent.ACTION_VIEW, uri);
             startActivity(browser);
         }
-
     }
 }
