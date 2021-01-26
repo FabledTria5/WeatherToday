dc8aaeaf7893984a8b6f1255202b96c995608a0f
Алексей Рогатин <bamako99@mail.ru>
2021-01-26 18:47:36 +0300

    Изменено хранение API ключа

--- app/src/main/java/com/example/weathertoday/network/WeatherGetter.java
+++ app/src/main/java/com/example/weathertoday/network/WeatherGetter.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.weathertoday.BuildConfig;
 import com.example.weathertoday.fragments.MainFragment;
 import com.example.weathertoday.network.model.WeatherRequest;
 import com.google.gson.Gson;
@@ -26,11 +27,11 @@ public class WeatherGetter {
     private static final String REQUEST_FIRST_PART = "https://api.openweathermap.org/data/2.5/weather?q=";
     private static final String UNITS = "&units=metric";
     private static final String LOCALE = "&lang=ru";
-    private static final String API_KEY = "&appid=f5f6ea48efb1914ff6e0b2857c54af1d";
+    private static final String API_KEY_PREFIX = "&appid=";
 
     public static void getWeather(String city, MainFragment parent) {
         try {
-            URL url = new URL(REQUEST_FIRST_PART + city + UNITS + LOCALE + API_KEY);
+            URL url = new URL((REQUEST_FIRST_PART + city + UNITS + LOCALE + API_KEY_PREFIX + BuildConfig.WEATHER_API_KEY));
             Handler handler = new Handler(Looper.getMainLooper());
             new Thread(() -> {
                 HttpsURLConnection urlConnection;
@@ -41,7 +42,6 @@ public class WeatherGetter {
                     BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                     String result = getLines(in);
 
-                    Log.d("123", result);
                     Gson gson = new Gson();
                     WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
                     handler.post(() -> parent.showWeather(weatherRequest));
